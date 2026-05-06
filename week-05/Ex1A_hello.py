42d6b3da7bc026e7b1f75c82260336661de2318a
LAEV415 <laev415@gmail.com>
2026-05-06 10:56:16 -0700

    Completed Ex1A

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,24 @@
 print('Hello World!')
+
+
+message = 'Hello World!'
+print(message)
+
+# 'Hello World!' prints out twice because of our original print statement and then
+# Our second print statement prints the value of our variable which is 'Hello World!'
+
+
+# Displaying dollars and cents
+
+dollars = 3
+cents = .50
+
+print(dollars + cents) 
+# I notice that our result was a successful addition of the two with the output being a float value
+cents = cents + .25
+print(dollars + cents)
+
+d_str = '3 dollars'
+c_str = '50 cents'
+print(d_str, c_str) # or print(d_str + " " + c_str)
+
