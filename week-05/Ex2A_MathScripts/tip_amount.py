533962aefd3448ed0fb77b1a0ee4ea0d35a48b50
LAEV415 <laev415@gmail.com>
2026-05-08 10:48:11 -0700

    modified tip_amount.py to include user input // completed Lab 3

--- week-05/Ex2A_MathScripts/tip_amount.py
+++ week-05/Ex2A_MathScripts/tip_amount.py
@@ -1,3 +1,6 @@
+#Script has been modified to answer 2A Lab 3
+
+
 # 3. How do you calculate the tip amount on a restaurant bill given the tip percentage?
 
 # a) Figure out the formula and what the script would look like, making up example
@@ -9,30 +12,54 @@
 
 # b) Create the script in a file named tip_amount.py
 
-# Food items available at taco truck
-tacos = 3.00
-burrito = 11.50
-quesadilla = 4.00
-carne_asada_plate = 14.50
-water_bottle = 1.00
-soda = 2.50
-agua_fresca = 4.50
-chips = 1.25
+# Food items available at taco truck and prices
+menu = {
+    "tacos": 3.00, 
+    "burrito": 11.50, 
+    "quesadilla": 4.00, 
+    "carne_asada_plate": 14.50, 
+    "water_bottle": 1.00, 
+    "soda": 2.50, 
+    "agua_fresca": 4.50, 
+    "chips": 1.25
+}
+# Initially used a list and tuple however caused values stored in 'order' to be strings 
+# rather than ints. This resulted in calculations not being able to be completed
 
+print('Menu for today:\n {}'.format(menu))
 # Order tax
 tax = 1.10
 
 # Collecting order
-order = [3 * tacos, agua_fresca, chips]
+order = []
+
+food = ''
+print('Hi welcome! What would you like to order? (input one food item and hit enter. ' \
+'Then add next food item. Input "done" when finished ordering) ')
+
+#Loop to allow multiple entries
+while food != 'done':
+    food = input()
+    food = food.lower()
+
+    if food in menu:             
+        order.append(menu[food]) 
+    elif food == 'done':
+        print('Calculating order price')
+    else:
+        print('Sorry try again.')
+
 
 # Collecting order total
 order_total = round(sum(order) * tax, 2)
+print(f'Order total is: {order_total}')
 
 # Non-optional Standard tip --> 15%
-tip = .15
+tip = int(input('How much % would you like to tip? '))
+tip = tip / 100
 
 #Calculating tip
-tip_total = round(order_total * tip, 2)
+tip_total = round(order_total * tip, 3)
 
 # c) The displayed output should be formatted as follows:
 # The tip on a $[number] restaurant bill is $[number]
